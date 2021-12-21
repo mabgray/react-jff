f5976174ab98957b03ebfb4d8e8ab30b33da1873
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2021-12-21 13:09:47 -0600

    getting set up with react router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
